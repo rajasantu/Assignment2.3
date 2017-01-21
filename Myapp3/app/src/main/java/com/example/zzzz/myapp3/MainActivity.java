b856a35d2fb79ec653e075fbf3a02ab5dc982a9b
rajasantu <raja.santu@gmail.com>
2017-01-21 19:40:11 +0530

    Assignment2.3
    Assignment2.3

---
+++ Myapp3/app/src/main/java/com/example/zzzz/myapp3/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.zzzz.myapp3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         super.onCreateOptionsMenu(menu);
+        menu.add("Exit");
+        return true;
+
+
+    }
+}
